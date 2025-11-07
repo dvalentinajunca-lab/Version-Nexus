61f5c2d7537fad38cef66a60a15aca9cf6092340
Danna Valentina Junca <dvalentinajunca@gmail.com>
2025-11-06 23:37:55 -0500

    Actualiza dark mode y navbar

--- src/hooks/useDarkMode.jsx
+++ src/hooks/useDarkMode.jsx
@@ -8,6 +8,12 @@ export const useDarkMode = () => {
 
   useEffect(() => {
     localStorage.setItem("darkMode", JSON.stringify(darkMode));
+    // 👉 Aquí es donde haces visible el cambio:
+    if (darkMode) {
+      document.body.classList.add("dark");
+    } else {
+      document.body.classList.remove("dark");
+    }
   }, [darkMode]);
 
   const toggleDarkMode = () => setDarkMode(!darkMode);
